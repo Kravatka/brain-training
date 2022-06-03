43e18440cfe0cd8fd102be748b24375f2e2ebdb8
Yehor Kravchenko <yehor.kravchenko@bigcommerce.com>
2022-06-03 23:32:42 +0300

    Valid Parentheses

---
+++ PHP/Easy/ValidParentheses/Solution.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ValidParentheses;
+
+class Solution
+{
+    function isValid(string $s): bool
+    {
+        $opened = [];
+
+        foreach (str_split($s) as $c) {
+            if (in_array($c, str_split('([{'))) {
+                $opened[] = $c;
+            } else if ($c == ')') {
+                if (array_pop($opened) !== '(') {
+                    return false;
+                }
+            } else if ($c == ']') {
+                if (array_pop($opened) !== '[') {
+                    return false;
+                }
+            } else if ($c == '}') {
+                if (array_pop($opened) !== '{') {
+                    return false;
+                }
+            }
+        }
+
+        return count($opened) == 0;
+    }
+}
+
+$s = new Solution();
+
+var_dump($s->isValid('()[]{}'));
+var_dump($s->isValid('(]'));
