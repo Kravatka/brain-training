999a8d70707a291f48ca9fdc6c53c5203ea78cf1
Yehor Kravchenko <yehor.kravchenko@bigcommerce.com>
2022-06-05 00:00:31 +0300

    Remove Element

---
+++ PHP/Easy/RemoveElement/Solution.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+class Solution
+{
+    function removeElement(array &$nums, int $val): int
+    {
+        while (array_search($val, $nums)) {
+            unset($nums[array_search($val, $nums)]);
+        }
+
+        return count($nums);
+    }
+}
+
+$s = new Solution();
+$nums = [0,1,2,2,3,0,4,2];
+var_dump($s->removeElement($nums, 2));
+$s->removeElement($nums, 2);
+var_dump($nums);
