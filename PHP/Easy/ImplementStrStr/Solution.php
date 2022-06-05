48e6f5ddac9a987454350b1d79b764339333cbe3
Yehor Kravchenko <yehor.kravchenko@bigcommerce.com>
2022-06-05 22:37:06 +0300

    Implement strStr()

---
+++ PHP/Easy/ImplementStrStr/Solution.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+class Solution
+{
+    function strStr(string $haystack, string $needle): int
+    {
+        $str_len = strlen($haystack);
+        $needle_len = strlen($needle);
+        $i = 0;
+        $j = 0;
+        $k = 0;
+
+        while ($i < $str_len) {
+            if ($haystack[$i] === $needle[$j]) {
+                while ($j < $needle_len) {
+                    $k++;
+                    $j++;
+                    if (!isset($haystack[$i + $k]) || !isset($needle[$j])) {
+                        break;
+                    }
+                    if ($haystack[$i + $k] !== $needle[$j]) {
+                        break;
+                    }
+                }
+                if ($j === $needle_len) {
+                    return $i;
+                }
+                $k = 0;
+                $j = 0;
+            }
+            $i++;
+        }
+        return -1;
+    }
+}
+
+$s = new Solution();
+
+var_dump($s->strStr('hello', 'll'));
